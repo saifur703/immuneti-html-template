5c61fa79b173e9eb9e09de9c96bb39045d50485b
Saifur Rahman <saifur703@gmail.com>
2020-11-06 14:39:41 +0600

    Initial Deploy

---
+++ js/active.js
@@ -0,0 +1,56 @@
+;
+(function ($) {
+    $(document).ready(function ($) {
+
+        $(".mobile-trigger").on("click", function () {
+            $(".main-menu").toggleClass("active");
+            $(".header-menu").toggleClass("active");
+        });
+        $(".main-menu .menu-close").on("click", function (e) {
+            e.preventDefault();
+            $(".main-menu").toggleClass("active");
+            $(".header-menu").toggleClass("active");
+        });
+
+        $('.featured-slider').owlCarousel({
+            items: 4,
+            margin: 30,
+            loop: false,
+            dots: false,
+            nav: true,
+            navText: ["<i class='fas fa-angle-left'></i>", "<i class='fas fa-angle-right'></i>"],
+            autoplay: false,
+            responsive: {
+                0: {
+                    items: 2,
+                },
+                480: {
+                    items: 2,
+                },
+                768: {
+                    items: 3
+                },
+                992: {
+                    items: 4
+                },
+                1200: {
+                    items: 4
+                }
+
+            }
+        });
+
+        $('a[href*="#"]:not([href="#"])').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top-180
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+})(jQuery);
